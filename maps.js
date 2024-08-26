fde33db0bf1ce050db8f18c4d91f6e8d35d42aa2
WinterWizad <158757945+WinterWizad@users.noreply.github.com>
2024-08-26 11:48:21 +0545

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,12 +1,17 @@
-const button=document.querySelector("#button")
-const answer=document.querySelector(".answer")
+const button = document.querySelector("#button");
+const answer = document.querySelector(".answer");
 
-function success(position){
-    answer.innerHTML=`{position}`;
+function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    answer.innerHTML = `Latitude: ${latitude}, Longitude: ${longitude}`;
 }
-function fail(error){
-      console.log(error)
+
+function fail(error) {
+    console.log(error);
+    answer.innerHTML = `Error: ${error.message}`;
 }
-button.addEventListener("click", async()=>{
-    navigator.geolocation.getCurrentPosition(success(position),fail(error))
-})
+
+button.addEventListener("click", async () => {
+    navigator.geolocation.getCurrentPosition(success, fail);
+});
